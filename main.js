3af0a0494c42aa2ccf68799c6fbc3e93f862d71f
ArisGoi <aris.goi97@gmail.com>
2021-09-07 15:07:35 +0200

    add B-1

--- main.js
+++ main.js
@@ -17,3 +17,26 @@ var studente01 = {
 for (var key in studente01){
     console.log(studente01[key])
 };
+
+// ------------------------------------------------------------------------------ //
+
+// B-1) Creare un array di oggetti di studenti.
+var studenti = [
+    {
+        "nome":"Berardina",
+        "cognome":"Gracci",
+        "eta":"35",
+    },
+    {
+        "nome":"Urania",
+        "cognome":"Redolfi",
+        "eta":"30",
+    },
+    {
+        "nome":"Erido",
+        "cognome":"Pollino",
+        "eta":"36",
+    },
+];
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
